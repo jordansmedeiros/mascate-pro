6464a3ed580605e96761b956183b9f4a4e63812f
Jordan Medeiros <jordan.medeiros@sinesys.com.br>
2025-09-19 20:48:06 -0300

    server

--- server.js
+++ server.js
@@ -299,7 +299,7 @@ app.get('/api/activity-logs', async (req, res) => {
 
 // Catch-all handler: send back React's index.html file in production
 if (process.env.NODE_ENV === 'production') {
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
   });
 }
